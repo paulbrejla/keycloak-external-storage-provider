d433d04320a08d7570a6558ebd988ad48d195e81
paulbrejla <paul@paulbrejla.com>
2025-05-05 19:46:30 +0200

    adds publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/paulbrejla/keycloak-rest-spi")
+            url = uri("https://maven.pkg.github.com/paulbrejla/keycloak-external-storage-provider")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
